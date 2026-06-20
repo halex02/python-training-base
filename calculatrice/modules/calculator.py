937d73252534032fe06792687ab832fbf4416967
Alexandre Moret <halex02@msn.com>
2026-06-20 11:12:00 +0200

    correction typo

--- calculatrice/modules/calculator.py
+++ calculatrice/modules/calculator.py
@@ -2,21 +2,21 @@
 # -*- coding: utf-8 -*-
 
 class DivisionByZeroError(Exception):
-    """Raised when typing to divide by zero."""
+    """Raised when trying to divide by zero."""
 
-def add(a:float, b:float)->float:
+def add(a: float, b: float) -> float:
     """Return the sum of a and b."""
     return a + b
 
-def substract(a:float, b:float)->float:
+def subtract(a: float, b: float) -> float:
     """Return the difference between a and b."""
     return a - b
 
-def multiply(a:float, b:float)->float:
+def multiply(a: float, b: float) -> float:
     """Return the product of a and b."""
     return a * b
 
-def divide(a:float, b:float)->float:
+def divide(a: float, b: float) -> float:
     """Return a divided by b."""
     if b == 0:
         raise DivisionByZeroError("Division by zero is not allowed.")
